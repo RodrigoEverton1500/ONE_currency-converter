4e6dc60bcd3a9dce33a4048c2da2c3cb8a9164e1
Rodrigo Everton <rodrigoeverton1500@gmail.com>
2025-05-16 15:48:07 -0300

    can input values to calculate, attempt at error handling using api's json error message

--- src/Main.java
+++ src/Main.java
@@ -12,10 +12,15 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        Double value = 0.0;
         System.out.print("Digite código da moeda: ");
         String currencyID = scanner.nextLine();
 
         String address = "https://v6.exchangerate-api.com/v6/3ecb8af07f9216332d83b17f/latest/" + currencyID.toUpperCase();
+
+        System.out.print("Digite valor: ");
+        value = scanner.nextDouble();
+        scanner.nextLine();
         try {
             HttpClient client = HttpClient.newHttpClient();
             HttpRequest request = HttpRequest.newBuilder().uri(URI.create(address)).build();
@@ -25,11 +30,21 @@ public class Main {
             Gson gson = new GsonBuilder().create();
             CurrencyMap currencyMap = gson.fromJson(response.body(), CurrencyMap.class);
 
+            if(currencyMap.result().equals("error")) {
+                String errorType = (String) currencyMap.error_type();
+                System.out.println(errorType);
+                return;
+            }
+
             System.out.print("Digite código da moeda: ");
             currencyID = scanner.nextLine();
 
-            Double value = (Double) currencyMap.conversion_rates().get(currencyID.toUpperCase());
-            System.out.println(currencyMap.base_code() + "/" + currencyID.toUpperCase() + " = " + value);
+            Double base_value = (Double) currencyMap.conversion_rates().get(currencyID.toUpperCase());
+            if(base_value == null) {
+                base_value = 0.0;
+            }
+            value *= base_value;
+            System.out.printf("valor * " + currencyMap.base_code() + "/" + currencyID.toUpperCase() + " = %.2f", value);
         } catch (Exception exception) {
             System.out.println(exception.toString());
         }
--- src/models/CurrencyMap.java
+++ src/models/CurrencyMap.java
@@ -2,5 +2,8 @@ package models;
 
 import java.util.Map;
 
-public record CurrencyMap(String result, String error_type, String base_code, Map<String, Double> conversion_rates) {
-}
+public record CurrencyMap(String result,
+                          @JsonProperty("error-type")String error_type,
+                          String base_code,
+                          Map<String, Double> conversion_rates)
+{}
